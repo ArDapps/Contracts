58a1c5fa1a8693f8b023bc92a7db0139c46a0569
benjaminsalon <benjaminsalon.business@gmail.com>
2022-09-03 13:27:20 +0200

    Draft of Escrow

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0 license
 pragma solidity ^0.8.16;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./interfaces/IOffer.sol";
 
 contract ZKRent_Escrow {
+
+    // Public Personnal details
+    string name;
+    string selfDescription;
+
+
     struct Offer {
         string title;
         uint256 id;
@@ -9,15 +19,29 @@ contract ZKRent_Escrow {
         uint256 timestampApplied;
         address beneficiary;
         uint256 amountForEscrow;
+        address offerAddress;
     }
 
+    IERC20 DAI;
+
+    uint256[] ids;
     mapping (uint256 => Offer) offerIDtoOffer;
 
-    function applyToOffer(uint256 _offerID) public {
-        fund(_offerID);
+    constructor(address _DAIAddress, string memory _name, string memory _selfDescription) {
+        DAI = IERC20(_DAIAddress);
+        name = _name;
+        selfDescription = _selfDescription;
     }
 
-    function fund(uint256 _offerID) public {
+    function applyToOffer(uint256 _offerID, uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[10] memory input) public {
+        Offer memory offerToApply = offerIDtoOffer[_offerID];
         
+        // registration on the offer, the verification is done on the offer side.
+        IOffer(offerToApply.offerAddress).registerApplicant(name,selfDescription, a,b,c,input);
     }
+
+    // TODO The locking of the funds on the escrow
+
+
+
 }
--- contracts/Offer.sol
+++ contracts/Offer.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.16;
 
 import "./Verifier.sol";
 import "./interfaces/IOffer.sol";
+import "./interfaces/IEscrow.sol";
 
 contract Offer is IOffer, Verifier {
 
@@ -10,6 +11,9 @@ contract Offer is IOffer, Verifier {
     address public owner;
     uint public incomeRequirement;
     string public description;
+
+    // Time for approval
+    uint constant TIME_FOR_ACCEPTANCE = 3 days;
 
     //Applicants structure
     struct Applicant {
@@ -21,17 +25,27 @@ contract Offer is IOffer, Verifier {
     //List of the applicants that applied successfully
     Applicant[] public applicants;
 
-    // One applicant is chosen at some point
+    // One applicant is chosen at some point OR WE CAN PUT THAT IN THE APPLICANT STRUCT
     Applicant public chosenApplicant;
     bool public isApplicantChosen = false;
+    uint deadlineForAcceptance;
 
     // We choose the applicant via the index in the array
     function chooseApplicant(uint index) public {
         require(!isApplicantChosen, "Applicant already choosen");
         isApplicantChosen = true;
         chosenApplicant = applicants[index];
+        deadlineForAcceptance = block.timestamp + TIME_FOR_ACCEPTANCE;
     }
 
-    // WARNING TIME WINDOW NOT IMPLEMENTED
+    function registerApplicant(string memory _name, string memory _description,  uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[10] memory input) external {
+        bool isProofValid = verifyProof(a, b, c, input);
+        require(isProofValid, "Invalid proof");
+        Applicant memory applicant;
+        applicant.escrowAddress = msg.sender;
+        applicant.name = _name;
+        applicant.description = _description;
+        applicants.push(applicant);
+    }
 
 }
